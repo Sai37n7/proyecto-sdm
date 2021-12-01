48a829a3be98e8a80db0428639d1f9addccf1e0c
Isai Hernandez <isai.12hernandez@gmail.com>
2021-12-01 00:01:25 -0600

    Navbar corregido

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container">
                     <a className="navbar-brand" href="/#"><b>SDM</b></a>
-                    <button className="navbar-toggler m-0" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
